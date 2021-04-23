5350e1013d3bab727986b517bcfe6f73d5469601
moez87 <harrabimoez87@gmail.com>
2021-04-23 23:04:28 +0100

    fix companny crud

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -180,36 +180,36 @@ export const navItems: NavData[] = [
   //     }
   //   ]
   // },
-  // {
-  //   name: 'Forms',
-  //   url: '/forms',
-  //   icon: 'icon-note',
-  //   children: [
-  //     {
-  //       name: 'Basic Forms',
-  //       url: '/forms/basic-forms',
-  //       icon: 'icon-note'
-  //     },
-  //     {
-  //       name: 'Advanced',
-  //       url: '/forms/advanced-forms',
-  //       icon: 'icon-note',
-  //       badge: {
-  //         variant: 'danger',
-  //         text: 'PRO'
-  //       }
-  //     },
-  //     {
-  //       name: 'Validation',
-  //       url: '/forms/validation-forms',
-  //       icon: 'icon-note',
-  //       badge: {
-  //         variant: 'danger',
-  //         text: 'PRO'
-  //       }
-  //     },
-  //   ]
-  // },
+  {
+    name: 'Forms',
+    url: '/forms',
+    icon: 'icon-note',
+    children: [
+      {
+        name: 'Basic Forms',
+        url: '/forms/basic-forms',
+        icon: 'icon-note'
+      },
+      {
+        name: 'Advanced',
+        url: '/forms/advanced-forms',
+        icon: 'icon-note',
+        badge: {
+          variant: 'danger',
+          text: 'PRO'
+        }
+      },
+      {
+        name: 'Validation',
+        url: '/forms/validation-forms',
+        icon: 'icon-note',
+        badge: {
+          variant: 'danger',
+          text: 'PRO'
+        }
+      },
+    ]
+  },
   // {
   //   name: 'Google Maps',
   //   url: '/google-maps',
