2678fb580a82cc512b4d6af45a0a7cd9d5c36796
moez87 <harrabimoez87@gmail.com>
2021-04-28 14:56:33 +0100

    fix company

--- src/app/views/company/company-routing.module.ts
+++ src/app/views/company/company-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompanyComponent } from './company.component';
 import { AddCompanyComponent } from './components/add-company/add-company.component';
-import { ListCompanyComponent } from './components/list-company/list-company.component';
 import { UpdateCompanyComponent } from './components/update-company/update-company.component';
+
 
 const routes: Routes = [
   {
