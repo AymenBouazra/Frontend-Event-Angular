318bd3f6f79a15b787d12bd8cb59b49174246faf
moez87 <harrabimoez87@gmail.com>
2021-04-29 12:11:15 +0100

    delted unused resourse

--- src/app/views/company/services/company.service.ts
+++ src/app/views/company/services/company.service.ts
@@ -5,25 +5,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CompanyService {
-  updateCompanyDataByid(updatecompanyData: any, id: any) {
-    return this.http.put("http://localhost:3000/company/"+id,updatecompanyData)
-  }
-  DeleteCompanyById(i: any) {
-    throw new Error('Method not implemented.');
-  }
 
-  constructor(private http:HttpClient) { }
-  postCompany(companyData:any)
-  {
-   return this.http.post("http://localhost:3000/company",companyData)
+  constructor(private http: HttpClient) { }
+  postCompany(companyData: any) {
+    return this.http.post("http://localhost:3000/company", companyData)
   }
-  getcompanyByid(id:number){
-    return this.http.get("http://localhost:3000/company/"+id)
+  getcompanyByid(id: number) {
+    return this.http.get("http://localhost:3000/company/" + id)
   };
-  getAllCompnanies(){
+  getAllCompnanies() {
     return this.http.get("http://localhost:3000/company")
   };
-  deleteCompanyById(id:number){
-    return this.http.delete("http://localhost:3000/company/"+id)
+  updateCompanyDataByid(updatecompanyData: any, id: any) {
+    return this.http.put("http://localhost:3000/company/" + id, updatecompanyData)
+  }
+  deleteCompanyById(id: number) {
+    return this.http.delete("http://localhost:3000/company/" + id)
   };
 }
