66c43687e2d597a754c7def31d88dddcb62a35d9
moez87 <harrabimoez87@gmail.com>
2021-05-06 10:16:25 +0100

    fix event

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -68,62 +68,62 @@ export const navItems: NavData[] = [
   //   url: '/theme/typography',
   //   icon: 'icon-pencil'
   // },
-  // {
-  //   title: true,
-  //   name: 'Components'
-  // },
-  // {
-  //   name: 'Base',
-  //   url: '/base',
-  //   icon: 'icon-puzzle',
-  //   children: [
-  //     {
-  //       name: 'Cards',
-  //       url: '/base/cards',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Carousels',
-  //       url: '/base/carousels',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Collapses',
-  //       url: '/base/collapses',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Pagination',
-  //       url: '/base/paginations',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Popovers',
-  //       url: '/base/popovers',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Progress',
-  //       url: '/base/progress',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Switches',
-  //       url: '/base/switches',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Tabs',
-  //       url: '/base/tabs',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Tooltips',
-  //       url: '/base/tooltips',
-  //       icon: 'icon-puzzle'
-  //     }
-  //   ]
-  // },
+  {
+    title: true,
+    name: 'Components'
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    icon: 'icon-puzzle',
+    children: [
+      {
+        name: 'Cards',
+        url: '/base/cards',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Carousels',
+        url: '/base/carousels',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Collapses',
+        url: '/base/collapses',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Pagination',
+        url: '/base/paginations',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Popovers',
+        url: '/base/popovers',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Progress',
+        url: '/base/progress',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Switches',
+        url: '/base/switches',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Tabs',
+        url: '/base/tabs',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Tooltips',
+        url: '/base/tooltips',
+        icon: 'icon-puzzle'
+      }
+    ]
+  },
   {
     name: 'Buttons',
     url: '/buttons',
